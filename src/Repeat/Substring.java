ff67541284b6962e47c04136024c8b4632b62a4e
Saena <toastyhatesthesea@gmail.com>
2018-07-06 16:27:20 -0700

    CheckForRepeating1

--- src/Repeat/Substring.java
+++ src/Repeat/Substring.java
@@ -4,4 +4,19 @@ public class Substring
 {
 
 
+    /**
+     * Checks for any repeating characters by checking the last character throughout the String
+     * @param input String
+     * @return boolean
+     */
+    public boolean checkForRepeating(String input)
+    {
+        boolean answer = false;
+        String lastChar = input.substring(input.length() - 1, input.length());
+
+        return answer;
+    }
+
+
+
 }
