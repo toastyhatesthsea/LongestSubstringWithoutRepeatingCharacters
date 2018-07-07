990754025d2ba99ee37cc43060a2af28e518800e
Saena <toastyhatesthesea@gmail.com>
2018-07-07 00:07:51 -0700

    Redoing LengthOf2

--- src/Repeat/Substring.java
+++ src/Repeat/Substring.java
@@ -6,10 +6,38 @@ public class Substring
 
     public int lengthOfLongestSubstring(String s) {
 
+
+        int charCount = 0;
+        int highest = 0;
+        int currentIndex = 0;
+
+        if (s.length() == 1)
+        {
+            return s.length();
+        }
+        else
+        {
+            for (int i = 0; i < s.length(); i++)
+            {
+                String beginningChar = s.substring(i, i + 1);
+                boolean foundDuplicate = false;
+                for (int j = i + 1; j < s.length() && !foundDuplicate; j++)
+                {
+                    charCount++;
+                    String currentChar = s.substring(j, j + 1);
+
+                    if (beginningChar.equals(currentChar))
+                    {
+                        foundDuplicate = true;
+                        if(charCount - 1 >)
+                    }
+                }
+            }
         }
 
-        return highest;
-    }
+        }
+
+
 
 
     /**
