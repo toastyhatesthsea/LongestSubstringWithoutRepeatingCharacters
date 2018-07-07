3a6adb9b59b6d6614f4f867eb5e710ad1fc5676e
Saena <toastyhatesthesea@gmail.com>
2018-07-06 23:48:45 -0700

    Redoing LengthOf

--- src/Repeat/Substring.java
+++ src/Repeat/Substring.java
@@ -5,56 +5,7 @@ public class Substring
 
 
     public int lengthOfLongestSubstring(String s) {
-        int currentLength = 1;
-        int highest = currentLength;
-        int startingIndex = 0;
-        int endingIndex = 1;
-        String aSubstring = "";
 
-        if (s.length() == 1)
-        {
-            return currentLength;
-        }
-        else
-        {
-            boolean repeats = false;
-            boolean done = false;
-            while (startingIndex < s.length() - 1 && !done)
-            {
-                while (!repeats && (endingIndex + 1) <= s.length())
-                {
-                    aSubstring = s.substring(startingIndex, endingIndex + 1);
-                    repeats = checkForRepeating(aSubstring);
-                    endingIndex++;
-                }
-
-                if (startingIndex >= s.length() - 1 && !repeats)
-                {
-                    currentLength = aSubstring.length();
-                }
-                else if (aSubstring.length() >= s.length() - startingIndex && repeats)
-                {
-                    currentLength = aSubstring.length() - 1;
-                    done = true;
-                }
-                else if (s.length() == aSubstring.length())
-                {
-                    currentLength = aSubstring.length();
-                }
-                else
-                {
-                    currentLength = aSubstring.length() - 1;
-                }
-
-                if (currentLength > highest)
-                {
-                    highest = currentLength;
-                }
-                startingIndex++;
-                endingIndex = startingIndex;
-                endingIndex++;
-                repeats = false;
-            }
         }
 
         return highest;
@@ -93,7 +44,7 @@ class SubstringTesters
     {
         Substring rawrs = new Substring();
 
-        int answer = rawrs.lengthOfLongestSubstring("abc");
+        int answer = rawrs.lengthOfLongestSubstring("abccdefg");
 
     }
 
