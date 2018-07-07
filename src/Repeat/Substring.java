b850c7a2e9d69b0881f7cb37050d167f38858ccd
Saena <toastyhatesthesea@gmail.com>
2018-07-07 01:17:51 -0700

    Using Hashmap for CheckforRepeating2

--- src/Repeat/Substring.java
+++ src/Repeat/Substring.java
@@ -1,5 +1,7 @@
 package Repeat;
 
+import java.util.HashMap;
+
 public class Substring
 {
 
@@ -44,7 +46,24 @@ public class Substring
      */
     public int checkForRepeating(String input, int startingIndex)
     {
+        HashMap<String, Integer> rawrs = new HashMap<>();
+        boolean foundRepeat = false;
+        int repeatIndexLocation = startingIndex;
 
+        for (int i = 0; i < input.length() && !foundRepeat; i++)
+        {
+            String currentChar = input.substring(i, i + 1);
+
+            if (rawrs.containsKey(currentChar))
+            {
+                repeatIndexLocation = i;
+                foundRepeat = true;
+            } else
+            {
+                rawrs.put(currentChar, 1);
+            }
+        }
+        return repeatIndexLocation;
     }
 
 }
